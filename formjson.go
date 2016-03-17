451871bff6667471fb3ff0a50ef54a9e7ba8e606
Boone Putney <boone@soleer.com>
2016-03-17 06:32:52 -0500

    initial commit

---
+++ formjson.go
@@ -0,0 +1,62 @@
+// Package formjson provides Middleware for converting posted x-www-form-urlencoded data into json
+package formJson
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"mime"
+	"net/http"
+	"strings"
+
+	"github.com/ant0ine/go-json-rest/rest"
+)
+
+//Middleware for converting posted form data into json
+type ConvertFormData struct{}
+
+func (mw *ConvertFormData) MiddlewareFunc(handler rest.HandlerFunc) rest.HandlerFunc {
+
+	return func(w rest.ResponseWriter, r *rest.Request) {
+
+		mediatype, _, _ := mime.ParseMediaType(r.Header.Get("Content-Type"))
+		if mediatype == "application/x-www-form-urlencoded" {
+			// get body
+			buf, _ := ioutil.ReadAll(r.Body)
+
+			// map body form data
+			jsonMap := map[string]string{}
+			sections := strings.Split(string(buf), "&")
+			for _, sectionValue := range sections {
+				sectionParts := strings.Split(sectionValue, "=")
+				if len(sectionParts) == 2 {
+					jsonMap[sectionParts[0]] = sectionParts[1]
+				} else {
+					//error converting, skip to handler
+					mw.conversionError(w)
+					return
+				}
+			}
+
+			//marshal json
+			jsonString, err := json.Marshal(jsonMap)
+			if err != nil {
+				//error marshalling, skip to handler
+				mw.conversionError(w)
+				return
+			}
+
+			//write new body
+			r.Body = ioutil.NopCloser(bytes.NewReader([]byte(string(jsonString))))
+
+			//convert content-type header
+			r.Header.Set("Content-Type", "application/json")
+		}
+		// call the wrapped handle
+		handler(w, r)
+	}
+}
+
+func (mw *ConvertFormData) conversionError(w rest.ResponseWriter) {
+	rest.Error(w, "Error Converting Form Data", http.StatusInternalServerError)
+}
---
+++ formjson_test.go
@@ -0,0 +1,95 @@
+package formJson
+
+import (
+	"bytes"
+	"net/http"
+	"net/url"
+	"strconv"
+	"testing"
+
+	"github.com/ant0ine/go-json-rest/rest"
+	"github.com/ant0ine/go-json-rest/rest/test"
+)
+
+type JSON map[string]interface{}
+
+func simpleGetEndpoint(w rest.ResponseWriter, r *rest.Request) {
+	body := map[string]interface{}{
+		"status": "success",
+	}
+	w.WriteJson(body)
+}
+
+func simplePostEndpoint(w rest.ResponseWriter, r *rest.Request) {
+	var body map[string]interface{}
+	r.DecodeJsonPayload(&body)
+	w.WriteJson(body)
+}
+
+func NewSimpleAPI(convertFormData *ConvertFormData) http.Handler {
+	api := rest.NewApi()
+	api.Use(convertFormData)
+	router, _ := rest.MakeRouter(
+		rest.Post("/", simplePostEndpoint),
+		rest.Get("/", simpleGetEndpoint),
+	)
+	api.SetApp(router)
+	return api.MakeHandler()
+}
+
+func TestPostValidFormData(t *testing.T) {
+	handler := NewSimpleAPI(&ConvertFormData{})
+
+	data := url.Values{}
+	data.Set("name", "foo")
+	data.Add("surname", "bar")
+
+	r, _ := http.NewRequest("POST", "http://localhost/", bytes.NewBufferString(data.Encode())) // <-- URL-encoded payload
+	r.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	r.Header.Add("Content-Length", strconv.Itoa(len(data.Encode())))
+
+	recordedPost := test.RunRequest(t, handler, r)
+
+	recordedPost.CodeIs(http.StatusOK)
+	recordedPost.BodyIs(`{"name":"foo","surname":"bar"}`)
+}
+
+func TestPostInvalidFormData(t *testing.T) {
+	handler := NewSimpleAPI(&ConvertFormData{})
+
+	data := "mal&formed"
+
+	r, _ := http.NewRequest("POST", "http://localhost/", bytes.NewBufferString(data)) // <-- URL-encoded payload
+	r.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	r.Header.Add("Content-Length", strconv.Itoa(len(data)))
+
+	recordedPost := test.RunRequest(t, handler, r)
+
+	recordedPost.CodeIs(http.StatusInternalServerError)
+	recordedPost.BodyIs(`{"Error":"Error Converting Form Data"}`)
+}
+
+func TestGet(t *testing.T) {
+	handler := NewSimpleAPI(&ConvertFormData{})
+
+	req := test.MakeSimpleRequest("GET", "http://localhost/", nil)
+	recorded := test.RunRequest(t, handler, req)
+
+	recorded.CodeIs(http.StatusOK)
+	recorded.BodyIs(`{"status":"success"}`)
+}
+
+func TestPostJSON(t *testing.T) {
+	handler := NewSimpleAPI(&ConvertFormData{})
+
+	simplePostData := map[string]interface{}{
+		"john": "doe",
+		"age":  30,
+	}
+
+	postRequest := test.MakeSimpleRequest("POST", "https://localhost/", simplePostData)
+	recordedPost := test.RunRequest(t, handler, postRequest)
+
+	recordedPost.CodeIs(http.StatusOK)
+	recordedPost.BodyIs(`{"age":30,"john":"doe"}`)
+}
